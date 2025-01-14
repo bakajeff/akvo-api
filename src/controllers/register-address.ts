0ef8723c39037b6893658c38d6db9eda32a4fe3c
jeffte <32723610+bakajeff@users.noreply.github.com>
2025-01-14 15:13:06 -0300

    feat: add register address controller

---
+++ src/controllers/register-address.ts
@@ -0,0 +1,39 @@
+import { zValidator } from "@hono/zod-validator";
+import { createId } from "@paralleldrive/cuid2";
+import { PrismaClient } from "@prisma/client";
+import { Hono } from "hono";
+import { z } from "zod";
+
+const prisma = new PrismaClient();
+
+const app = new Hono();
+
+app.post(
+	"/",
+	zValidator(
+		"json",
+		z.object({
+			name: z.string().min(1),
+			latitude: z.number(),
+			longitude: z.number(),
+		}),
+	),
+	async (c) => {
+		const { sub: customerId } = c.get("jwtPayload");
+		const { name, latitude, longitude } = c.req.valid("json");
+
+		const customer = await prisma.address.create({
+			data: {
+				id: createId(),
+				name,
+				latitude,
+				longitude,
+				userId: customerId,
+			},
+		});
+
+		return c.json(customer);
+	},
+);
+
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -15,6 +15,7 @@ import dispatchOrder from "@/controllers/dispatch-order";
 import getOrderDetails from "@/controllers/get-order-details";
 import getOrders from "@/controllers/get-orders";
 import getProfile from "@/controllers/get-profile";
+import registerAddress from "@/controllers/register-address";
 import registerCustomer from "@/controllers/register-customer";
 import registerStore from "@/controllers/register-store";
 import sendAuthenticationLink from "@/controllers/send-authentication-link";
@@ -46,6 +47,7 @@ app.route("/orders", approveOrder);
 app.route("/orders", cancelOrder);
 app.route("/orders", dispatchOrder);
 app.route("/orders", deliverOrder);
+app.route("/addresses", registerAddress);
 
 serve(app, (info) => {
 	console.log(`🔥 Server running on port ${info.port}`);
